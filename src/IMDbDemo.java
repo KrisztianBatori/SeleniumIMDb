810478c5f476db062b588e2e91382429e654736f
KrisztianBatori <batori.krisztian97@gmail.com>
2019-04-02 16:26:39 +0200

    feat (IMDbDemo): navigates to the login page

--- src/IMDbDemo.java
+++ src/IMDbDemo.java
@@ -5,5 +5,7 @@ public class IMDbDemo {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.imdb.com");
+        ((ChromeDriver) driver).findElementByCssSelector("#imdb-signin-link").click();
+        ((ChromeDriver) driver).findElementByCssSelector("#signin-options > div > div:nth-child(2) > a:nth-child(1)").click();
     }
 }
