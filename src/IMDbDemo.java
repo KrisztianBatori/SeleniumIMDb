0714bcc1842f6c4732c98a95eedd755bac972cb3
KrisztianBatori <batori.krisztian97@gmail.com>
2019-04-02 16:48:06 +0200

    feat (IMDbdEMO): logins a user

--- src/IMDbDemo.java
+++ src/IMDbDemo.java
@@ -7,5 +7,8 @@ public class IMDbDemo {
         driver.get("https://www.imdb.com");
         ((ChromeDriver) driver).findElementByCssSelector("#imdb-signin-link").click();
         ((ChromeDriver) driver).findElementByCssSelector("#signin-options > div > div:nth-child(2) > a:nth-child(1)").click();
+        ((ChromeDriver) driver).findElementByCssSelector("#ap_email").sendKeys(args[0]);
+        ((ChromeDriver) driver).findElementByCssSelector("#ap_password").sendKeys(args[1]);
+        ((ChromeDriver) driver).findElementByCssSelector("#signInSubmit").click();
     }
 }
