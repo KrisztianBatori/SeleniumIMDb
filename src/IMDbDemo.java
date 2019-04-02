39fb6b527207831677b474a59be825b6692aa27d
KrisztianBatori <batori.krisztian97@gmail.com>
2019-04-02 16:20:19 +0200

    feat (IMDbDemo): opens imdb.com

--- src/IMDbDemo.java
+++ src/IMDbDemo.java
@@ -1,5 +1,9 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class IMDbDemo {
     public static void main(String[] args) {
-
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.imdb.com");
     }
 }
