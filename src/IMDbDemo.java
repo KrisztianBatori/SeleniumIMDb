5c07c527276ae97e5ede0d29c4c3732fe3b93920
KrisztianBatori <batori.krisztian97@gmail.com>
2019-04-03 09:19:32 +0200

    refac(main): constructor and login method

--- src/IMDbDemo.java
+++ src/IMDbDemo.java
@@ -2,13 +2,25 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class IMDbDemo {
-    public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.imdb.com");
+
+    private WebDriver driver = new ChromeDriver();
+    private String mainPage = "https://www.imdb.com";
+
+    IMDbDemo() {
+        driver.get(mainPage);
+    }
+
+    public void login() {
         ((ChromeDriver) driver).findElementByCssSelector("#imdb-signin-link").click();
         ((ChromeDriver) driver).findElementByCssSelector("#signin-options > div > div:nth-child(2) > a:nth-child(1)").click();
-        ((ChromeDriver) driver).findElementByCssSelector("#ap_email").sendKeys(args[0]);
-        ((ChromeDriver) driver).findElementByCssSelector("#ap_password").sendKeys(args[1]);
+        ((ChromeDriver) driver).findElementByCssSelector("#ap_email").sendKeys(System.getProperty("USER_EMAIL"));
+        ((ChromeDriver) driver).findElementByCssSelector("#ap_password").sendKeys(System.getProperty("USER_PASSWORD"));
         ((ChromeDriver) driver).findElementByCssSelector("#signInSubmit").click();
+    }
+
+
+    public static void main(String[] args) {
+        IMDbDemo demo = new IMDbDemo();
+        demo.login();
     }
 }
