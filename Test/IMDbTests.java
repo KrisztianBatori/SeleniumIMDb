c3d0e7941b8b40791f618b66fe01b7e41ee04234
KrisztianBatori <batori.krisztian97@gmail.com>
2019-04-03 10:13:01 +0200

    feat(login, checkIfRightUserHasLogged, closeBrowser): testing a simple login process

--- Test/IMDbTests.java
+++ Test/IMDbTests.java
@@ -1,5 +1,25 @@
-import org.junit.Test;
+import org.junit.*;
 
 public class IMDbTests {
 
+    private IMDbDemo demo = new IMDbDemo(); // Open a chrome browser.
+
+    @Before
+    public void login() {
+        System.out.println("LOG INTO THE IMDB ACCOUNT");
+        demo.login();
+    }
+
+    @Test
+    public void checkIfRightUserHasLogged() {
+        System.out.println("CHECK IF THE RIGHT USER HAS LOGGED");
+        Assert.assertTrue(demo.checkUser(System.getProperty("USER_NAME")));
+        System.out.println("THE RIGHT USER HAS LOGGED");
+    }
+
+    @After
+    public void closeBrowser() {
+        System.out.println("CLOSE BROWSER");
+        demo.closeBrowser();
+    }
 }
