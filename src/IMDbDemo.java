d7234322dd89c6f3cf7f277866449674236e8ba2
KrisztianBatori <batori.krisztian97@gmail.com>
2019-04-03 15:11:49 +0200

    fix(searchResult): method now functions properly

--- src/IMDbDemo.java
+++ src/IMDbDemo.java
@@ -1,8 +1,8 @@
+import jdk.jfr.Timespan;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import java.util.concurrent.TimeUnit;
 
 public class IMDbDemo {
@@ -32,10 +32,34 @@ public class IMDbDemo {
     }
 
     public void searchResult(String word, int nthResult) {
-        ((JavascriptExecutor) driver).executeScript("document.querySelector('#navbar-suggestionsearch').style.display=\"block\"");
-        driver.findElement(By.cssSelector("#navbar-query")).sendKeys(word);
-        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS) ;
-        driver.findElement(By.cssSelector(String.format("#navbar-suggestionsearch > div:nth-child(%s) > a", nthResult))).click();
+        int failTreshold = 3;
+        int numberOfFails = 0;
+        while (true) {
+            try {
+                ((JavascriptExecutor) driver).executeScript(String.format("document.getElementById(\"navbar-query\").value = \"%s\"", word));
+                driver.findElement(By.cssSelector("#navbar-query")).click();
+                driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                driver.findElement(By.cssSelector(String.format("#navbar-suggestionsearch > div:nth-child(%s) > a", nthResult))).click();
+                break;
+            } catch (org.openqa.selenium.StaleElementReferenceException e) {
+                numberOfFails += 1;
+                if (numberOfFails == failTreshold) {
+                    throw new ArithmeticException("Reached fail treshold in 'searchResult' method.");
+                }
+                driver.findElement(By.cssSelector("#navbar-query")).clear();
+            }
+        }
+    }
+
+    public boolean checkTitleAndRatings(String title, String ratings) {
+        return driver.findElement(
+                By.cssSelector("#title-overview-widget > div.vital > div.title_block > div > div.titleBar > " +
+                        "div.title_wrapper > h1")
+                ).getText().contains(title) &&
+                driver.findElement(
+                        By.cssSelector("#title-overview-widget > div.vital > div.title_block > div > " +
+                                "div.ratings_wrapper > div.imdbRating > div.ratingValue > strong > span")
+                ).getText().equals(ratings);
     }
 
     public void closeBrowser() {
@@ -46,5 +70,8 @@ public class IMDbDemo {
         IMDbDemo demo = new IMDbDemo();
         demo.login(System.getProperty("USER_EMAIL"), System.getProperty("USER_PASSWORD"));
         demo.searchResult("game", 1);
+        System.out.println(
+                demo.checkTitleAndRatings("Game of Thrones", "9.5")
+        );
     }
 }
